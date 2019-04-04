e978195a0491c5449cb0122fe97f14a93bc02c46
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2019-04-03 20:03:50 -0400

    Add files via upload

---
+++ contactForm.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['send'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
+
+  $mailTo = "INSERT EMAIL HERE";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailSend");
+}
