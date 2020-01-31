a012e52034c5878a423168978c5a0c22c0316093
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2020-01-31 12:57:36 +0000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ NOTE: This is the custom jQuery file for the template
         var element = $(".element");
         $(function() {
             element.typed({
-                strings: ["Graphic Designer", "Public Speaker", "Analyst", "Cofounder", "Develper-to-be"],
+                strings: ["Graphic Designer", "Public Speaker", "Analyst", "Cofounder", "Developer-to-be"],
                 typeSpeed: 100,
 								backDelay: 1000,
                 loop: true,
