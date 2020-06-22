0bfe8573c657a75183c7b495fdf8aceea4cd6bd2
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2020-06-21 21:16:01 -0400

    Updated Blog Posts and Compressed Images

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,7 +132,7 @@ NOTE: This is the custom jQuery file for the template
         /*======== Preloder ===========*/
        setTimeout(function() {
             $('body').addClass('loaded');
-        }, 2500);
+        }, 0);
 
     });
 
